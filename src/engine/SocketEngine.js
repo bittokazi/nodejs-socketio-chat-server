bc7bccd5d261ab5b829c86eabb30dd3735592e35
Bitto Kazi <bitto.kazi@gmail.com>
2021-08-18 14:01:45 +0600

    Update SocketEngine.js

--- src/engine/SocketEngine.js
+++ src/engine/SocketEngine.js
@@ -5,10 +5,11 @@ import io from "socket.io";
 export default function SocketEngine(app) {
   const httpApp = http.createServer(app);
   const socketIo = io(httpApp, { 
+    origins: ["https://pmbt.bitto.website,https://chat-pmbt.bitto.website"],
     handlePreflightRequest: (req, res) => {
       res.writeHead(200, {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET,POST",
+        "Access-Control-Allow-Origin": "https://chat-pmbt.bitto.website,https://pmbt.bitto.website",
+        "Access-Control-Allow-Methods": "*",
         "Access-Control-Allow-Headers": "*",
         "Access-Control-Allow-Credentials": true
       });
