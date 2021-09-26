993fe7b035f52c716e2a084f22cffdb6f92bc2cf
bittokazi <bitto.kazi@gmail.com>
2021-09-26 21:57:24 +0600

    socket.io cors fix

--- src/engine/SocketEngine.js
+++ src/engine/SocketEngine.js
@@ -5,17 +5,7 @@ import io from "socket.io";
 export default function SocketEngine(app) {
   const httpApp = http.createServer(app);
   const socketIo = io.listen(httpApp, {
-    handlePreflightRequest: (req, res) => {
-      res.writeHead(200, {
-        "Access-Control-Allow-Origin": req.headers.origin,
-        "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
-        "Access-Control-Allow-Headers":
-          "Origin, X-Requested-With, Content-Type, Accept, Referer, User-Agent, Host, Authorization",
-        "Access-Control-Allow-Credentials": true,
-        "Access-Control-Max-Age": 8640000,
-      });
-      res.end();
-    },
+    cors: { origin: "*" },
   });
   SocketIoHandlers(socketIo);
   return httpApp;
