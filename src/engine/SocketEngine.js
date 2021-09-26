51f8832fba46ef2ca8d5ad36bf4fb198eb031c54
bittokazi <bitto.kazi@gmail.com>
2021-09-26 21:35:22 +0600

    socket.io cors fix

--- src/engine/SocketEngine.js
+++ src/engine/SocketEngine.js
@@ -4,7 +4,11 @@ import io from "socket.io";
 
 export default function SocketEngine(app) {
   const httpApp = http.createServer(app);
-  const socketIo = io.listen(httpApp);
+  const socketIo = io.listen(httpApp, {
+    cors: {
+      origin: ["*"],
+    },
+  });
   SocketIoHandlers(socketIo);
   return httpApp;
 }
