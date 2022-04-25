5abb9843204ab982b840641855eee6a68db61c42
bittokazi <bitto.kazi@gmail.com>
2022-04-25 18:30:40 +0200

    bug on user status

--- src/socketio/@connection-events/ConnectionEvents.js
+++ src/socketio/@connection-events/ConnectionEvents.js
@@ -4,7 +4,21 @@ export default function ConnectionEvents(
   user,
   userOnlineStatus
 ) {
-  socket.conn.on("heartbeat", () => {});
+  socket.conn.on("heartbeat", () => {
+    if (userOnlineStatus[`${user.tenant}/${user.id}`])
+      userOnlineStatus[`${user.tenant}/${user.id}`].forEach((element) => {
+        socketIo
+          .to(`${user.tenant}/${element.roomUid}`)
+          .emit("chat.user.status", {
+            user: {
+              id: user.id,
+              roomUid: element.roomUid,
+              status: 0,
+            },
+          });
+        promises.push(element);
+      });
+  });
 
   socket.on("disconnect", () => {
     let promises = [];
